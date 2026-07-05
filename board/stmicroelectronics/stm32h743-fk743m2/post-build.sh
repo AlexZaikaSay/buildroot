c1323e816a4724a809bf7abe146792bc3b18c3e1
Alex Zaika <sayfuture@gmail.com>
2026-07-05 18:21:14 +0300

    enabled uart, no correct init proc

--- board/stmicroelectronics/stm32h743-fk743m2/post-build.sh
+++ board/stmicroelectronics/stm32h743-fk743m2/post-build.sh
@@ -7,3 +7,13 @@ BOARD_DIR="$(dirname "$0")"
 sed -i '/^devpts/d' "${TARGET_DIR}"/etc/fstab
 
 install -m 0644 -D "${BOARD_DIR}"/extlinux.conf "${TARGET_DIR}"/boot/extlinux/extlinux.conf
+
+
+# # Add a console on ttySTM0
+# if [ -e ${TARGET_DIR}/etc/inittab ]; then
+# 	# Remove default generic console getty entry.
+# 	sed -i '/^console::respawn:/d' ${TARGET_DIR}/etc/inittab
+# 	grep -qE '^ttySTM0::' ${TARGET_DIR}/etc/inittab || \
+# 		sed -i '/GENERIC_SERIAL/a\
+# ttySTM0::respawn:/sbin/getty -L ttySTM0 115200 vt100' ${TARGET_DIR}/etc/inittab
+# fi
